d2939a5f1705ae4b1cfe68a005dbfddcc4309fce
Hiromune Ito <muniere.fujiwara@gmail.com>
2019-12-27 01:13:21 +0900

    Remove unnecessary closure

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,37 +1,33 @@
-(() => {
-  'use strict';
+//
+// Installation
+//
+chrome.runtime.onInstalled.addListener(details => {
+  console.log('previousVersion', details.previousVersion);
+});
 
-  //
-  // Installation
-  //
-  chrome.runtime.onInstalled.addListener(details => {
-    console.log('previousVersion', details.previousVersion);
-  });
+//
+// Context Menu
+//
+chrome.contextMenus.create({
+  type: 'normal',
+  title: 'Open Results',
+  contexts: ['page'],
+  documentUrlPatterns: [
+    'https://www.google.co.jp/*',
+    'https://www.google.com/*',
+  ],
+  onclick: (_info, tab) => {
+    chrome.tabs.sendMessage(tab.id, { action: 'scrape' }, (result) => {
+      if (!result.success) {
+        return console.info('failed to perform scrape', result);
+      }
+      if (!Array.isArray(result.data.hrefs)) {
+        return console.info('result.hrefs is not an array', result);
+      }
 
-  //
-  // Context Menu
-  //
-  chrome.contextMenus.create({
-    type: 'normal',
-    title: 'Open Results',
-    contexts: ['page'],
-    documentUrlPatterns: [
-      'https://www.google.co.jp/*',
-      'https://www.google.com/*',
-    ],
-    onclick: (_info, tab) => {
-      chrome.tabs.sendMessage(tab.id, { action: 'scrape' }, (result) => {
-        if (!result.success) {
-          return console.info('failed to perform scrape', result);
-        }
-        if (!Array.isArray(result.data.hrefs)) {
-          return console.info('result.hrefs is not an array', result);
-        }
-
-        result.data.hrefs.forEach((url) => {
-          chrome.tabs.create({ url: url, active: false });
-        });
+      result.data.hrefs.forEach((url) => {
+        chrome.tabs.create({ url: url, active: false });
       });
-    }
-  });
-})();
+    });
+  }
+});
